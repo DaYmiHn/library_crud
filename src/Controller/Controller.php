34dccc1277a80c13ed250d4eaa28b0812a17db75
Denisn Savin <sawi.denis@yandex.ru>
2020-02-18 23:38:33 +0300

    teig & stole Bootstrap template

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -3,10 +3,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class Controller
+class Controller extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
@@ -15,8 +16,13 @@ class Controller
     {
         $num = random_int(0, 100);
 
-        return new Response(
-            '<html><body>'.$num.'</body></html>'
-        );
+//        return new Response(
+//            '<html><body>'.$num.'</body></html>'
+//        );
+        $number = random_int(0, 100);
+
+        return $this->render('base.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
