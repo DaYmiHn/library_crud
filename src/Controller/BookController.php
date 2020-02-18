8f2ec629f7cea46c02b7f5147b47af49b5ea5443
Denisn Savin <sawi.denis@yandex.ru>
2020-02-19 01:39:12 +0300

    demo api crud

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -21,6 +21,43 @@ class BookController extends AbstractController
         return $this->render('book/index.html.twig', [
             'books' => $books
         ]);
-//        return new Response('Check out this great product: '.$book->getName());
+    }
+    /**
+     * @Route("/add/{name}/{year}/{author}", name="add_book")
+     */
+    public function addBook($name,$year,$author){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $book = new Book();
+        $book->setName($name);
+        $book->setYear($year);
+        $book->setAuthor($author);
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+        return new Response('Saved new book with id '.$book->getId());
+    }
+    /**
+     * @Route("/update/{id}/{name}/{year}/{author}", name="update_book")
+     */
+    public function updateAction(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $book = $entityManager->getRepository(Book::class)->find(2);
+        $book->setName('OmG!!!');
+        $entityManager->flush();
+
+        return new Response('Update book with id '.$book->getId());
+    }
+    /**
+     * @Route("/delete/{id}", name="delete_book")
+     */
+    public function deleteAction(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $book = $entityManager->getRepository(Book::class)->find(5);
+        $id = $book->getId();
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return new Response('Delete book with id '. $id);
     }
 }
